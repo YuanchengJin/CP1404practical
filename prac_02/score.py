2ccbf89b5152f619e2d3262a501536a23f319205
Yuancheng_Jin <yuancheng.jin@my.jcu.edu.au>
2024-06-03 09:15:13 +0800

    ADD THE MAGIC NUMBER

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,7 +2,10 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
+MINI_SCORE = 0
+MAX_SCORE = 100
+PASS_THREEFOLD = 50
+EXCELLENT_SCORE = 90
 def main():
     # ask the user for their score and print the result.
     score = get_score()
@@ -13,11 +16,11 @@ def get_score():
     return score
 def print_score(score):
     # analyse the score and print the result
-    if score < 0 or score > 100:
+    if score < MINI_SCORE or score > MAX_SCORE:
         print("Invalid score")
-    elif score < 50:
+    elif score < PASS_THREEFOLD:
         print("Bad")
-    elif score < 90:
+    elif score < EXCELLENT_SCORE:
         print("Passable")
     else:
         print('Excellent')

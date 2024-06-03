ac6f3b90211f85cd2eb72b3e86470207df49c399
Yuancheng_Jin <yuancheng.jin@my.jcu.edu.au>
2024-06-03 09:11:58 +0800

    ADD MAGIC NUMBER

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -2,14 +2,17 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
+MINI_SCORE = 0
+MAX_SCORE = 100
+EXCELLENT_SCORE = 90
+PASS_THRESHOLD = 50
 
 score = float(input("Enter score: "))
-if score < 0 or score > 100:
+if score < MINI_SCORE or score > MAX_SCORE:
     print("Invalid score")
-elif score < 50:
+elif score < PASS_THRESHOLD:
     print("Bad")
-elif score < 90:
+elif score < EXCELLENT_SCORE:
     print("Passable")
 else:
     print('Excellent')

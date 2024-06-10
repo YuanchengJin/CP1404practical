49199fec0b0c654f75480884596e9b6ac0b05507
Yuancheng_Jin <yuancheng.jin@my.jcu.edu.au>
2024-06-10 09:13:27 +0800

    submit to prac3

--- practicals/prac_03/capitalist_conrad.py
+++ practicals/prac_03/capitalist_conrad.py
@@ -36,5 +36,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
     number_of_days +=1
     price *= (1 + price_change)
     print(f"On day {number_of_days} price is:${price:,.2f} ", file=out_file)
+    print(f"On day {number_of_days} price is:${price:,.2f} ")
 
 out_file.close()

79c998af4fb8d41b96bbac4fab569f6f2461f89e
Yuancheng_Jin <yuancheng.jin@my.jcu.edu.au>
2024-06-15 23:15:30 +0800

    submit to prac3

--- practicals/prac_04/list_comprehension.py
+++ practicals/prac_04/list_comprehension.py
@@ -29,21 +29,17 @@ print(a_names)
 # 'Ada Alan Angel Bob Jimi'
 print(" ".join(sorted(names)))
 
-# TODO: list comprehension to create a list of all the full_names in lowercase format
 lowercase_full_names = [name.lower() for name in full_names]
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
-# TODO: list comprehension to create a list of integers from the above list of strings
 int_numbers = [int(number) for number in almost_numbers]
 # print(int_numbers)
 
 
-# TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
 numbers = [number for number in int_numbers if number > 9]
 print(numbers)
 
 
-# TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 last_name = [name.split(' ',1)[1]  for name in full_names if len(name) > 11]
 print(last_name)
